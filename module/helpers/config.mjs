5f2254527982badcf8dea8469d4bda774b9abb66
Parker Harris Emerson <parker.emerson@gmail.com>
2023-06-07 13:43:53 -0700

    v1.0.16
    Sweet 16. More work on skills.

--- module/helpers/config.mjs
+++ module/helpers/config.mjs
@@ -18,66 +18,105 @@ Flagship2d20.abilityAbbreviations = {
 
 Flagship2d20.SKILLS = [
   {
-    'label': 'ACADEMIA',
-    'key': "Academia",
-    'focuses': ['Art', 'Cryptography', 'Finance', 'History', 'Linguistics', 'Occultism', 'Science']
+    'label': 'CHARM',
+    'key': "Charm",
+    'focuses': ['Flirt', 'Perform', 'Revel']
   },
   {
-    'label': 'ATHLETICS',
-    'key': "Athletics",
-    'focuses': ['Climbing', 'Lifting', 'Physical Training', 'Running', 'Swimming', 'Throwing']
+    'label': 'CONNECTIONS',
+    'key': "Connections",
+    'focuses': ['Bureaucracy', 'Criminal', 'High', 'Low']
   },
   {
-    'label': 'ENGINEERING',
-    'key': "Engineering",
-    'focuses': ['Architecture', 'Combat Engineering', 'Electronics', 'Explosives', 'Mechanical Engineering']
+    'label': 'COOL',
+    'key': "Nerves",
+    'focuses': ['Discipline', 'Initiative']
   },
   {
-    'label': 'FIGHTING',
-    'key': "Fighting",
-    'focuses': ['Close Quarters', 'Handguns', 'Hand-to-Hand', 'Heavy Weapons', 'Melee Weapons', 'Rifles',
-      'Threat Awareness', 'Exotic']
+    'label': 'FIGHT',
+    'key': "Fight",
+    'focuses': ['Hand-to-Hand', 'Improvised Weapons', 'Melee Weapons']
   },
   {
-    'label': 'MEDICINE',
-    'key': "Medicine",
-    'focuses': ['First Aid', 'Infectious Diseases', 'Pharmacology', 'Psychiatry', 'Surgery', 'Toxicology']
+    'label': 'FIX',
+    'key': "Fix",
+    'focuses': ['Electronics', 'Explosives', 'Mechanical']
   },
   {
-    'label': 'OBSERVATION',
-    'key': "Observation",
-    'focuses': ['Hearing', 'Instincts', 'Sight', 'Smell and Taste']
+    'label': 'FORCE',
+    'key': "Force",
+    'focuses': ['Breaking', 'Lifting', 'Throwing']
   },
   {
-    'label': 'PERSUASION',
-    'key': "Persuasion",
-    'focuses': ['Charm', 'Innuendo', 'Intimidation', 'Negotiation', 'Rhetoric', 'Deceive', 'Invocation']
+    'label': 'HEAL',
+    'key': "Heal",
+    'focuses': ['Diagnosis', 'First Aid', 'Psychiatry', 'Surgery']
   },
   {
-    'label': 'RESILIENCE',
-    'key': "Resilience",
-    'focuses': ['Fortitude', 'Discipline', 'Immunity']
+    'label': 'INVESTIGATE',
+    'key': "Investigate",
+    'focuses': ['Forensics', 'Research', 'Searching']
+  },
+  {
+    'label': 'KNOW',
+    'key': "Know",
+    'focuses': ['Business', 'History', 'Language', 'Law', 'Science']
+  },
+  {
+    'label': 'LEAD',
+    'key': "Lead",
+    'focuses': ['Command', 'Inspire', 'Rhetoric']
+  },
+  {
+    'label': 'PERSUADE',
+    'key': "Persuade",
+    'focuses': ['Bargain', 'Deceive', 'Intimidate']
+  },
+  {
+    'label': 'PILOT',
+    'key': "Pilot",
+    'focuses': [  'Operations', 'Personal Craft', 'Shuttlecraft', 'Watercraft', 'Wheeled']
+  },
+  {
+    'label': 'RESIST',
+    'key': "Resist",
+    'focuses': ['Endure', 'Immunity', 'Pain']
+  },
+  {
+    'label': 'RESOURCEFULNESS',
+    'key': "Resourcefulness",
+    'focuses': ['Acquisitions', 'Forgery', 'Preparedness', 'Wealth']
+  },
+  {
+    'label': 'SENSE',
+    'key': "Sense",
+    'focuses': ['Hearing', 'Sight', 'Smell and Taste', 'Vigilance']
+  },
+  {
+    'label': 'SHOOT',
+    'key': "Shoot",
+    'focuses': ['Ancient Weapons', 'Close Quarters', 'Heavy Weapons', 'Pistols', 'Rifles']
   },
   {
     'label': 'STEALTH',
     'key': "Stealth",
-    'focuses': ['Camouflage', 'Disguise', 'Rural Stealth', 'Urban Stealth']
+    'focuses': ['Camoflage', 'Disguise', 'Wilderness Stealth', 'Urban Stealth']
+  },
+  {
+    'label': 'STUNT',
+    'key': "Stunt",
+    'focuses': ['Climbing', 'Jumping', 'Running', 'Swimming', 'Zero-G']
   },
   {
     'label': 'SURVIVAL',
     'key': "Survival",
-    'focuses': ['Animal Handling', 'Foraging', 'Hunting', 'Mysticism', 'Orienteering', 'Tracking']
+    'focuses': ['Animal Handling', 'Foraging', 'Hunting', 'Navigation', 'Tracking']
   },
   {
-    'label': 'TACTICS',
-    'key': "Tactics",
-    'focuses': ['Air Force', 'Army', 'Covert Operations', 'Leadership', 'Navy', 'Technical Projects']
-  },
-  {
-    'label': 'VEHICLES',
-    'key': "Vehicles",
-    'focuses': ['Cars', 'Motorcycles', 'Heavy Vehicles', 'Tanks', 'Aircraft', 'Watercraft']
-  }
+    'label': 'THIEVERY',
+    'key': "Thievery",
+    'focuses': ['Appraisal', 'Locks and Traps', 'Sleight of Hand']
+  }  
 ];
 
 Flagship2d20.Size = ["Trivial", "Minor", "Major"];
